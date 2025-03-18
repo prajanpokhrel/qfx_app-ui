e9117827919d950ae4e67d553f100fb127100b29
prajan pokhrel <prajanpokhrel09@gmail.com>
2025-03-18 16:20:45 +0545

    added right drawer

--- lib/features/homescreen/presentation/homescreen.dart
+++ lib/features/homescreen/presentation/homescreen.dart
@@ -9,79 +9,100 @@ class Homescreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 63, 61, 61),
-
-        toolbarHeight: 70, // Increases AppBar height
-        leadingWidth: 100,
-        leading: Padding(
-          padding: EdgeInsets.only(left: 3.h),
-          child: Image(
-            width: 10.w,
-            height: 8.h,
-            fit: BoxFit.contain,
-            image: AssetImage("assets/images/logo.png"),
+        key: scaffoldKey, // Assign the key to access scaffold functions
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 63, 61, 61),
+          toolbarHeight: 70, // Increases AppBar height
+          leadingWidth: 100,
+          leading: Padding(
+            padding: EdgeInsets.only(left: 3.h),
+            child: Image(
+              width: 10.w,
+              height: 8.h,
+              fit: BoxFit.contain,
+              image: AssetImage("assets/images/logo.png"),
+            ),
           ),
-        ),
-        title: Container(
-          child: DropdownMenuExample(),
-        ),
-        actions: [
-          Padding(
-            padding: EdgeInsets.only(right: 1.h),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  PageRouteBuilder(
-                    pageBuilder: (context, animation, secondaryAnimation) =>
-                        SearchScreen(
-                      title: 'Movies',
+          title: Container(
+            child: DropdownMenuExample(),
+          ),
+          actions: [
+            Padding(
+              padding: EdgeInsets.only(right: 1.h),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    PageRouteBuilder(
+                      pageBuilder: (context, animation, secondaryAnimation) =>
+                          SearchScreen(title: 'Movies'),
+                      transitionsBuilder:
+                          (context, animation, secondaryAnimation, child) {
+                        const begin = Offset(1.0, 0.0); // Slide from right
+                        const end = Offset.zero;
+                        const curve = Curves.easeInOut;
+
+                        var tween = Tween(begin: begin, end: end)
+                            .chain(CurveTween(curve: curve));
+                        var offsetAnimation = animation.drive(tween);
+
+                        return SlideTransition(
+                          position: offsetAnimation,
+                          child: child,
+                        );
+                      },
+                      transitionDuration:
+                          Duration(milliseconds: 600), // Animation duration
                     ),
-                    transitionsBuilder:
-                        (context, animation, secondaryAnimation, child) {
-                      const begin = Offset(1.0, 0.0); // Slide from right
-                      const end = Offset.zero;
-                      const curve = Curves.easeInOut;
-
-                      var tween = Tween(begin: begin, end: end)
-                          .chain(CurveTween(curve: curve));
-                      var offsetAnimation = animation.drive(tween);
-
-                      return SlideTransition(
-                        position: offsetAnimation,
-                        child: child,
-                      );
+                  );
+                },
+                child: Icon(
+                  Icons.search,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 2.h),
+              child: Builder(
+                builder: (context) {
+                  return GestureDetector(
+                    onTap: () {
+                      scaffoldKey.currentState?.openEndDrawer();
                     },
-                    transitionDuration:
-                        Duration(milliseconds: 600), // Animation duration
-                  ),
-                );
-              },
-              child: Icon(
-                Icons.search,
-                color: Colors.white,
+                    child: Icon(
+                      Icons.more_vert,
+                      color: Colors.white,
+                    ),
+                  );
+                },
               ),
             ),
+          ],
+        ),
+        endDrawer: Drawer(
+          backgroundColor: Color.fromARGB(255, 36, 35, 35),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 30.h, top: 4.h),
+                child: IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(
+                      Icons.arrow_forward_ios,
+                      color: const Color.fromARGB(255, 128, 126, 126),
+                    )),
+              )
+            ],
           ),
-          Padding(
-            padding: EdgeInsets.only(right: 2.h),
-            child: GestureDetector(
-              onTap: () {
-                // Scaffold.of(context).openEndDrawer();
-              },
-              child: Icon(
-                Icons.more_vert,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ],
-      ),
-      body: Column(
-        children: [],
-      ),
-    );
+        ),
+        body: Column(
+          children: [],
+        ));
   }
 }
--- lib/utils/drawer.dart
+++ lib/utils/drawer.dart
@@ -1,1 +1,54 @@
+import 'package:flutter/material.dart';
 
+class DrawerScreen extends StatelessWidget {
+  const DrawerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Right Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: () {
+                // Handle drawer item tap
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {
+                // Handle drawer item tap
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.help),
+              title: Text('Help'),
+              onTap: () {
+                // Handle drawer item tap
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
