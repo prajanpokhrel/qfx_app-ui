340f59f21e81687ce90e897af54cff6082a12ec0
prajan pokhrel <prajanpokhrel09@gmail.com>
2025-03-25 22:08:12 +0545

    fixed issue in offerscreen appbar

--- lib/features/offers/presentation/offer.dart
+++ lib/features/offers/presentation/offer.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:qfx_app/features/homescreen/presentation/homescreen.dart';
 import 'package:qfx_app/utils/widgets/drawer/drawer.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
@@ -11,58 +10,61 @@ class OffersScreen extends StatefulWidget {
 }
 
 class _OffersScreenState extends State<OffersScreen> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
-    final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
     return Scaffold(
       key: scaffoldKey,
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(80),
-        child: AppBar(
-          backgroundColor: const Color.fromARGB(255, 63, 61, 61),
-          elevation: 0, // Remove shadow for a flat appearance
-          leading: Padding(
-              padding: EdgeInsets.only(left: 4.w),
-              child: Image.asset("assets/images/logo.png")),
-
-          bottom: PreferredSize(
-            preferredSize: Size.fromHeight(40),
-            child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 2.h),
-                child: Row(
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      icon: Icon(
-                        Icons.arrow_back_ios,
-                        color: Colors.white,
-                      ),
+        preferredSize: Size.fromHeight(15.h),
+        child: SafeArea(
+          child: Container(
+            color: const Color.fromARGB(255, 63, 61, 61),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(right: 20.h),
+                  child: Image.asset(
+                    "assets/images/logo.png",
+                    width: 14.w,
+                    height: 6.h,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 4.w, vertical: 1.h),
+                    child: Row(
+                      children: [
+                        IconButton(
+                          onPressed: () => Navigator.pop(context),
+                          icon: const Icon(Icons.arrow_back_ios,
+                              color: Colors.white),
+                        ),
+                        const Spacer(),
+                        const Text(
+                          "OFFERS",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                        const Spacer(),
+                        IconButton(
+                          onPressed: () =>
+                              scaffoldKey.currentState?.openEndDrawer(),
+                          icon: const Icon(Icons.menu, color: Colors.white),
+                        ),
+                      ],
                     ),
-                    Spacer(),
-                    Text(
-                      "OFFERS",
-                      style: TextStyle(color: Colors.white, fontSize: 20),
-                    ),
-                    Spacer(),
-                    IconButton(
-                        onPressed: () {
-                          scaffoldKey.currentState?.openEndDrawer();
-                        },
-                        icon: Icon(
-                          Icons.menu,
-                          color: Colors.white,
-                        ))
-                  ],
-                )),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
       endDrawer: DrawerWidgets(),
-      body: Column(
-        children: [],
-      ),
+      body: const Column(children: []),
     );
   }
 }
